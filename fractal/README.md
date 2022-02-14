89d8a374fc5573e3bfcd984815ef7cdfad8e37f7
Yichen Mo <amomorning@gmail.com>
2022-02-14 14:00:21 +0800

    Add README

---
+++ fractal/README.md
@@ -0,0 +1,10 @@
+### Julia Set
+- pause animation with SPACE key
+
+![](imgs/2022-02-14-13-57-02.png)
+
+### Mandelbrot Set
+- move with UP, DOWN, LEFT, RIGHT keys
+- zoom and change iteration numbers with slider
+
+![](imgs/2022-02-14-13-55-47.png)
--- fractal/mandelbrot.py
+++ fractal/mandelbrot.py
@@ -5,6 +5,9 @@ n = 800
 pixels = ti.Vector.field(3, dtype=float, shape=(2*n, n))
 c = ti.Vector.field(2, dtype=float, shape=1)
 num = ti.field(dtype=int, shape=1)
+pos = ti.field(dtype=float, shape=2)
+pos[0] = -0.05
+pos[1] = -0.99
 
 @ti.func
 def complex_sqr(z):
@@ -30,10 +33,10 @@ def hsv_to_rgb(h: float, s: float, v: float) -> ti.Vector:
 
 
 @ti.kernel
-def paint(iter: int):
+def paint(iter: int, zoom: float):
     num[0] = iter
     for i, j in pixels: 
-        c[0] = ti.Vector([i/n-1.2, j/n-0.5])*2.5
+        c[0] = ti.Vector([i/n*zoom + pos[0], j/n*zoom + pos[1]])
         z = c[0]
         iterations = 0
         while(z.norm() < 20 and iterations < num[0]):
@@ -50,14 +53,32 @@ def paint(iter: int):
 gui = ti.GUI("Mandelbrot", res=(2*n , n))
 iter = gui.slider('Iterations', 5, 120, step=1)
 iter.value = 100
+zoom = gui.slider('Zoom', 0.1, 0, step=-0.0001)
+
+zoom.value = 0.005
 
 while gui.running:
 
-    paint(int(iter.value))
+    paint(int(iter.value), zoom.value)
     
     gui.set_image(pixels)
-    gui.text(content=f"Space: pause", pos=(0.01, 0.98), color=0x0)
+    gui.text(content=f"UP DOWN LEFT RIGHT", pos=(0.01, 0.98), color=0x0)
     gui.text(content=f"c = %.2f + %.2fi"%(c[0][0], c[0][1]), pos=(0.01, 0.95), color=0x0)
     gui.text(content=f"iterations = %d"%(num[0]), pos=(0.01, 0.92), color=0x0)
+
+    moved = 0.002
+
+    for e in gui.get_events(gui.PRESS):
+        if e.key == gui.ESCAPE:
+            gui.running = False
+        elif e.key == gui.RIGHT:
+            pos[0] += moved
+        elif e.key == gui.LEFT:
+            pos[0] -= moved
+        elif e.key == gui.UP:
+            pos[1] += moved 
+        elif e.key == gui.DOWN:
+            pos[1] -= moved
+        
 
     gui.show()
